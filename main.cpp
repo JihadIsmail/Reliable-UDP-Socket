3eaec07164a8d2720b3e5f48a5acee5aaf5c3934
Houssainy <mohamed.houssiany@gmail.com>
2014-12-11 18:02:22 +0200

    project packages initilized

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+#include "reliability_package/go_back_n.h"
+#include "reliability_package/R_UDP.h"
+
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+
     return 0;
 }

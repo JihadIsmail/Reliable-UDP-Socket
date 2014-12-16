fdf59663325ec8590def2302e161dd2d815ae3db
Jihad ISmail <nooneknow2050@gmail.com>
2014-12-16 18:00:59 +0200

    before pull

--- congestion_control_package/congestion_controller.cpp
+++ congestion_control_package/congestion_controller.cpp
@@ -1,11 +1,9 @@
 #include "congestion_controller.h"
 
-congestion_controller::congestion_controller()
-{
+congestion_controller::congestion_controller() {
     //ctor
 }
 
-congestion_controller::~congestion_controller()
-{
+congestion_controller::~congestion_controller() {
     //dtor
 }

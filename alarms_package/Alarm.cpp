9189ba4d2d6323593f358957b9b3fa142e7e410c
Ahmed Gaber <ahmedgaber20@gmail.com>
2014-12-16 22:33:10 +0200

    Alarm implementation tested

--- alarms_package/Alarm.cpp
+++ alarms_package/Alarm.cpp
@@ -1,11 +1,13 @@
 #include "Alarm.h"
 
 void Alarm::start(unsigned int time_out, unsigned int alarm_id) {
-  timer = thread(&Alarm::sleep, this, time_out, alarm_id);
+  timer = std::thread(&Alarm::sleep, this, time_out, alarm_id);
 }
 
 void Alarm::sleep(unsigned int time_out, unsigned int alarm_id) {
   is_stopped = false;
+  // TODO(houssainy) use unblocking sleep method to be able to wake up the
+  // thread instead instead of blocking closing the program!
   this_thread::sleep_for(std::chrono::milliseconds(time_out));
   if(listner != NULL && !is_stopped)
     listner->on_timeout(alarm_id);
